6137958e0cf6b46fb2970174aa9ef1a7637f6ec5
Hua Xie <x@wallyxie.com>
2020-12-10 19:21:55 -0800

    Updated function definitions

--- meta-analysis_replication_inference/sbm_meta-analysis_versions.py
+++ meta-analysis_replication_inference/sbm_meta-analysis_versions.py
@@ -9,9 +9,9 @@ def scon_pool_init_meta(I_S, I_D, varying_system_parameters, constant_system_par
     '''
     Determines and returns vector of values for parameters whose values depend on on proposed values for varying parameters, previously chosen values for other constant parameters, and choices of initial pre-warming steady state pool sizes, C_t0. This function assumes that the parameters that are varied and held constant are the same as for the inferences done in Xie et al., 2020. In Xie et al., 2020, steady state equations were re-arranged so that rather than pre-warming steady state pool sizes following from varying parameter values, pool sizes were pre-emptively set, which required some parameter values to be a priori held constant and others to be calculated from the re-arranged steady state equations following sampling of values for the varying system parameters at each Markov chain step. This was done to biologically constrain steady state pool sizes to biologically realistic values.
 
-    For SCON, varying system parameters need to be ordered by Ea_S, Ea_D, Ea_M, a_SD, a_DS, a_M, and a_MSC values. Constant system parameters need to be ordered by u_M values. Pre-warming values in the C_t0 array/vector/tensor need to be ordered by S0, D0, and M0.
+    For SCON, varying system parameters need to be ordered by Ea_S, Ea_D, Ea_M, a_{SD}, a_{DS}, a_M, and a_{MSC} values. Constant system parameters need to be ordered by u_M values. Pre-warming values in the C_t0 array/vector/tensor need to be ordered by S_0, D_0, and M_0.
 
-    For AWB, varying system parameters need to be ordered by ... values. Constant system parameters need to be ordered by ... values. Pre-warming values in the C_t0 array/vector/tensor need to be ordered by S0, D0, M0, and E0 values.
+    For AWB, varying system parameters need to be ordered by ... values. Constant system parameters need to be ordered by ... values. Pre-warming values in the C_t0 array/vector/tensor need to be ordered by S_0, D_0, M_0, and E_0 values.
     '''
     SDE_system = upper(SDE_system)
     if SDE_system == 'SCON':
@@ -28,10 +28,13 @@ def scon_pool_init_meta(I_S, I_D, varying_system_parameters, constant_system_par
             #Unpack constant system parameters.
             u_M = constant_system_parameters[0]
             #Unpack chosen pre-warming steady states.
-            S0 = C_t0[0]
-            D0 = C_t0[1]
-            M0 = C_t0[2]
-            #Solve for calculated parameter values to be plugged into equation solving function. 
+            S_0 = C_t0[0]
+            D_0 = C_t0[1]
+            M_0 = C_t0[2]
+            #Solve for calculated parameter values to be plugged into equation solving function.
+            k_M_ref = u_M * D_0 / M_0
+            k_S_ref = (I_S + D_0 * (a_DS * k_D_ref + u_M * a_M * a_MSC)) / S_0
+            k_D_ref = (-I_D - (a_SD * I_S) + (D_0 * u_M) - (a_M * D_0 * u_M) + (a_M * a_MSC * D_0 * u_M) - (a_M * a_MSC * a_SD * D_0 * u_M)) / (D_0 * (-1 + a_DS * a_SD))
         return k_S_ref, k_D_ref, k_M_ref #calculated system parameters
     elif SDE_system == 'SAWB':
         print('Checking to ensure expected number of varying system parameters (9), previously set system parameters (x), and pre-warming pool densities (4) for model SAWB are present.')
