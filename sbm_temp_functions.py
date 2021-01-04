755d6d29b0d06492a9d4f9a1fc0aa9ea4736ec4c
Hua Xie <x@wallyxie.com>
2021-01-04 15:46:01 -0800

    Update sbm_temp_functions.py

--- sbm_temp_functions.py
+++ sbm_temp_functions.py
@@ -5,6 +5,10 @@ import numpy as np
 ##Stochastic differential equation functions##
 ##############################################
 
+def temp_gen(t, temp_ref):
+    temp = temp_ref + t / (20 * 24 * 365) + 10 * np.sin((2 * np.pi / 24) * t) + 10 * np.sin((2 * np.pi / (24 * 365)) * t)
+    return temp
+
 def arrhenius_temp_dep(parameter, temp, Ea, temp_ref):
     '''
     For a parameter with Arrhenius temperature dependence, returns the transformed parameter value.
